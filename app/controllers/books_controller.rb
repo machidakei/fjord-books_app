67b35c1cdac18e4e8cef02316adf64aa4c9b845b
machidakei <mochiduki.mochizuki@gmail.com>
2022-03-06 17:32:08 +0900

    レビューを受け修正した

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,7 +6,7 @@ class BooksController < ApplicationController
   # GET /books
   # GET /books.json
   def index
-    @books = Book.all.order(created_at: :desc).page(params[:page]).per(3)
+    @books = Book.all.order(created_at: :desc).page(params[:page])
   end
 
   # GET /books/1
