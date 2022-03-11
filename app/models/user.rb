a6e9b30c19b879a484f9bb3a7947bbcafcdbbe15
machidakei <mochiduki.mochizuki@gmail.com>
2022-03-11 18:27:32 +0900

    ユーザーフォローを作った

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,21 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_one_attached :avatar
+  has_many :following_relationships, class_name: 'FollowRelationship', dependent: :destroy
+  has_many :follower_relationships, class_name: 'FollowRelationship', foreign_key: 'follow_id', dependent: :destroy, inverse_of: :user
+  has_many :followings, through: :following_relationships, source: :follow
+  has_many :followers, through: :follower_relationships, source: :user
+
+  def follow(other_user)
+    following_relationships.find_or_create_by(follow_id: other_user.id) unless self == other_user
+  end
+
+  def unfollow(other_user)
+    relationship = following_relationships.find_by(follow_id: other_user.id)
+    relationship&.destroy
+  end
+
+  def following?(other_user)
+    followings.include?(other_user)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,7 @@ Rails.application.routes.draw do
   root to: 'books#index'
   resources :books
   resources :users, only: %i(index show)
+  resources :follow_relationships, only: [:create, :destroy]
+  get '/users/:id/followings', to: 'follow_relationships#followings'
+  get '/users/:id/followers', to: 'follow_relationships#followers'
 end
