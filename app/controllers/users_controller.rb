bb772d7d70142545fa320400e7d96b94da3924ba
machidakei <mochiduki.mochizuki@gmail.com>
2022-03-13 10:03:44 +0900

    レビューを受け修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@
 
 class UsersController < ApplicationController
   def index
-    @users = User.order(:id).page(params[:page])
+    @users = User.order(:id).page(params[:page]).includes(:avatar)
   end
 
   def show
@@ -12,6 +12,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.fetch(:user, {}).permit(:name, :avatar)
+    params.require(:user).permit(:name, :avatar)
   end
 end
