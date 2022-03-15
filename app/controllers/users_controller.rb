238445246e07f9d46853acd2cb83418470c240be
machidakei <mochiduki.mochizuki@gmail.com>
2022-03-15 09:13:23 +0900

    修正対応した

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   before_action :set_user, only: %i[show edit update destroy]
 
   def index
-    @users = User.order(:id).page(params[:page]).per(3)
+    @users = User.order(:id).page(params[:page])
   end
 
   def show; end
